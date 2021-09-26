11261d3647f1c74ae5729a86847aba3c4b9e9548
luis <luis.uneng@gmail.com>
2021-09-26 21:23:05 +0800

    修改IndexController

---
+++ ForestBlog-springboot/src/main/java/cn/it/util/annotation/PassToken.java
@@ -0,0 +1,17 @@
+package cn.it.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author luis
+ * @date 2021/9/19 15:43
+ * 跳过验证
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PassToken {
+    boolean required() default true;
+}
---
+++ ForestBlog-springboot/src/main/java/cn/it/util/annotation/UserLoginToken.java
@@ -0,0 +1,17 @@
+package cn.it.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author luis
+ * @date 2021/9/19 15:44
+ * 需要登录才能进行操作的注解
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserLoginToken {
+    boolean required() default true;
+}
